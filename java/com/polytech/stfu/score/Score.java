d944164a4d105f8bfd21dd36b4d4171f9783980a
Adrien <adrien.coueron@wanadoo.fr>
2015-05-18 00:45:23 +0200

    Changement de dossier

--- java/com/polytech/stfu/score/Score.java
+++ java/com/polytech/stfu/score/Score.java
@@ -9,58 +9,58 @@ import java.util.TreeSet;
  *
  */
 public abstract class Score {
-    /**
-     * Fonction pour obtenir l'ensemble des meilleurs scores d'un mode
-     * @param mode Le mode de jeu voulu
-     * @return L'ensemble des 5 meilleurs scores
-     */
-    public static SortedSet<Couple> getHighScoreList(Mode mode, Context context){
-        SortedSet<Couple> ret = new TreeSet<Couple>();
+	/**
+	 * Fonction pour obtenir l'ensemble des meilleurs scores d'un mode
+	 * @param mode Le mode de jeu voulu
+	 * @return L'ensemble des 5 meilleurs scores
+	 */
+	public static SortedSet<Couple> getHighScoreList(Mode mode, Context context){
+		SortedSet<Couple> ret = new TreeSet<Couple>();
+		
+		SharedPreferences scores = context.getSharedPreferences("Scores", 0);
+		for(int i = 1; i<6; i++){
+			Couple tmp = new Couple(score.getString(mode.getNom()+"pseudo"+i, null), score.getInt(mode.getNom()+"score"+i, -1));
+			if(tmp.getDeux() == -1){
+				break;
+			}
+			ret.add(tmp);
+		}
+		return ret;
+	}
 
-        SharedPreferences scores = context.getSharedPreferences("Scores", 0);
-        for(int i = 1; i<6; i++){
-            Couple tmp = new Couple(score.getString(mode.getNom()+"pseudo"+i, null), score.getInt(mode.getNom()+"score"+i, -1));
-            if(tmp.getDeux() == -1){
-                break;
-            }
-            ret.add(tmp);
-        }
-        return ret;
-    }
+	/**
+	 * Fonction pour tester si un score est dans les 5 meileurs du mode voulu
+	 * @param mode Le mode de jeu voulu
+	 * @param score Le score a tester
+	 */
+	public static boolean isHighScore(Mode mode, int score, Context context){
+		SharedPreferences scores = getSharedPreferences("Scores", 0);
+		return score > scores.getInt(mode.getNom()+"score"+5, -1);
+	}
+	
+	/**
+	 * Fonction pour sauvegarder un couple (pseudo, score) dans le mode voulu.
+	 * L'ordre des meilleurs scores est assure
+	 * @param mode Le mode de jeu voulu
+	 * @param pseudo Le pseudo a sauvegarder
+	 * @param score Le score a sauvegarder
+	 */
+	public static void save(Mode mode, String pseudo, int score, Context context){
+		SharedPreferences scores = getSharedPreferences("Scores", 0);
+		SharedPreferences.Editor editor = scores.edit();
 
-    /**
-     * Fonction pour tester si un score est dans les 5 meileurs du mode voulu
-     * @param mode Le mode de jeu voulu
-     * @param score Le score a tester
-     */
-    public static boolean isHighScore(Mode mode, int score, Context context){
-        SharedPreferences scores = getSharedPreferences("Scores", 0);
-        return score > scores.getInt(mode.getNom()+"score"+5, -1);
-    }
-
-    /**
-     * Fonction pour sauvegarder un couple (pseudo, score) dans le mode voulu.
-     * L'ordre des meilleurs scores est assure
-     * @param mode Le mode de jeu voulu
-     * @param pseudo Le pseudo a sauvegarder
-     * @param score Le score a sauvegarder
-     */
-    public static void save(Mode mode, String pseudo, int score, Context context){
-        SharedPreferences scores = getSharedPreferences("Scores", 0);
-        SharedPreferences.Editor editor = scores.edit();
-
-        SortedSet<Couple> setScores = getHighScoreList(mode);
-        scores.add(new Couple(pseudo, score));
-
-        int i = 0;
-        for(Couple c : setScores){
-            if(i == 5){
-                break;
-            }
-            editor.putString(mode.getNom()+"pseudo"+i, c.getPseudo());
-            editor.putInt(mode.getNom()+"score"+i, c.getScore());
-            i++;
-        }
-        editor.commit();
-    }
+		SortedSet<Couple> setScores = getHighScoreList(mode);
+		scores.add(new Couple(pseudo, score));
+		
+		int i = 0;
+		for(Couple c : setScores){
+			if(i == 5){
+				break;
+			}
+			editor.putString(mode.getNom()+"pseudo"+i, c.getPseudo());
+			editor.putInt(mode.getNom()+"score"+i, c.getScore());
+			i++;
+		}
+	    editor.commit();
+	}
 }
