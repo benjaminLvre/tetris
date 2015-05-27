447cd132df189621b27bf35853ab9473d42302b6
Adrien <adrien.coueron@wanadoo.fr>
2015-05-27 21:16:04 +0200

    reglages

--- java/com/polytech/stfu/ihm/TetrisView.java
+++ java/com/polytech/stfu/ihm/TetrisView.java
@@ -161,25 +161,27 @@ public class TetrisView extends SurfaceView implements SurfaceHolder.Callback{
                                 case I: designCase = getResources().getDrawable(R.drawable.turquoiz_cube);break;
                                 default:break;
                             }break;
-                        case "walking_dead": switch (Jeu.getJeu().getGrille()[li][col]){
-                            case L : designCase = getResources().getDrawable(R.drawable.red_cube);break;
-                            case O: designCase = getResources().getDrawable(R.drawable.blue_cube);break;
-                            case S:designCase = getResources().getDrawable(R.drawable.green_cube);break;
-                            case Z: designCase = getResources().getDrawable(R.drawable.yellow_cube);break;
-                            case T: designCase = getResources().getDrawable(R.drawable.purple_cube);break;
-                            case J: designCase = getResources().getDrawable(R.drawable.orange_cube);break;
-                            case I: designCase = getResources().getDrawable(R.drawable.turquoiz_cube);break;
-                            default:break;
+                        case "walking_dead":
+                            switch (Jeu.getJeu().getGrille()[li][col]){
+                                case L : designCase = getResources().getDrawable(R.drawable.red_cube);break;
+                                case O: designCase = getResources().getDrawable(R.drawable.blue_cube);break;
+                                case S: designCase = getResources().getDrawable(R.drawable.green_cube);break;
+                                case Z: designCase = getResources().getDrawable(R.drawable.yellow_cube);break;
+                                case T: designCase = getResources().getDrawable(R.drawable.purple_cube);break;
+                                case J: designCase = getResources().getDrawable(R.drawable.orange_cube);break;
+                                case I: designCase = getResources().getDrawable(R.drawable.turquoiz_cube);break;
+                                default:break;
                         }break;
-                        default: switch (Jeu.getJeu().getGrille()[li][col]){
-                            case L : designCase = getResources().getDrawable(R.drawable.red_cube);break;
-                            case O: designCase = getResources().getDrawable(R.drawable.blue_cube);break;
-                            case S:designCase = getResources().getDrawable(R.drawable.green_cube);break;
-                            case Z: designCase = getResources().getDrawable(R.drawable.yellow_cube);break;
-                            case T: designCase = getResources().getDrawable(R.drawable.purple_cube);break;
-                            case J: designCase = getResources().getDrawable(R.drawable.orange_cube);break;
-                            case I: designCase = getResources().getDrawable(R.drawable.turquoiz_cube);break;
-                            default:break;
+                        default:
+                            switch (Jeu.getJeu().getGrille()[li][col]){
+                                case L : designCase = getResources().getDrawable(R.drawable.red_cube);break;
+                                case O: designCase = getResources().getDrawable(R.drawable.blue_cube);break;
+                                case S:designCase = getResources().getDrawable(R.drawable.green_cube);break;
+                                case Z: designCase = getResources().getDrawable(R.drawable.yellow_cube);break;
+                                case T: designCase = getResources().getDrawable(R.drawable.purple_cube);break;
+                                case J: designCase = getResources().getDrawable(R.drawable.orange_cube);break;
+                                case I: designCase = getResources().getDrawable(R.drawable.turquoiz_cube);break;
+                                default:break;
                         }break;
                     }
 
